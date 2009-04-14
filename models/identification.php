3ab5b107dd3e2ecd5e559f449f704d8abcd7b17b
Trever Fischer <wm161@wm161.net>
2009-04-14 09:44:57 -0700

    Moving towards 4-space indentation, added validates() function

--- models/identification.php
+++ models/identification.php
@@ -1,8 +1,21 @@
 <?php
 class Identification extends AppModel {
-  var $hasOne = array('Resident');
-  var $belongsTo = 'Person';
-  var $displayField = 'card_num';
-  var $recursive = 1;
+    var $hasOne = array('Resident');
+    var $belongsTo = 'Person';
+    var $displayField = 'card_num';
+    var $uses = array('Person');
+    var $recursive = 1;
+    var $validate = array('person_id'=>array('required'=>true, 'allowEmpty'=>false));
+    
+    public function validates($options) {
+        if (isset($this->data['person_id'])) {
+            $person = $this->data['person_id'];
+            if ($this->Person->findById($person)==null)
+            $this->invalidate('person_id');
+        } else {
+            $this->invalidate('person_id');
+        }
+        return count($this->invalidFields()) == 0;
+    }
 }
 ?>
