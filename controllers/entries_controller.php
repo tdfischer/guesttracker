c23cc6beb069e4203d132a12a3ec75677dced4ad
Trever Fischer <wm161@wm161.net>
2009-04-21 06:39:13 -0700

    EntryController: Implemented search() and create()

--- controllers/entries_controller.php
+++ controllers/entries_controller.php
@@ -3,10 +3,47 @@ class EntriesController extends AppController {
   var $scaffold;
   var $helpers = array('Ajax', 'Javascript', 'Widgets');
   var $components = array('Session');
-  var $uses = array('Entry', 'Person', 'Resident', 'Identification');
+  var $uses = array('Entry', 'Ban', 'Identification');
   
   public function create() {
     $resident = $this->Identification->findByCardNum($this->data['Resident']['card_num']);
+    //debug($resident);
+    if (!empty($resident['Resident'])) {
+        $valid = array();
+        $invalid = array();
+        $old = array();
+        $this->data['Entry']['resident_id'] = $resident['Resident']['id'];
+        foreach($this->data['Guest'] as $guest) {
+            $guest = $this->Identification->findByCardNum($guest['card_num']);
+            $oldentry = $this->Entry->find(array('person_id'=>$guest['Person']['id'], 'valid'=>'Y'));
+            if ($oldentry) {
+                $old[] = $guest;
+            } elseif ($guest) {
+                ///TODO: Search bans by building_id
+                //$this->Ban->find(array('person_id' => $guest['Person']['id'], 'building_id'=>));
+                $this->data['Entry']['person_id'] = $guest['Person']['id'];
+                $this->Entry->save($this->data);
+                $valid[] = $guest;
+            } else {
+                $invalid[] = $guest['card_num'];
+            }
+        }
+        if (!empty($old))
+            $this->Session->setFlash(__('Guest(s) already checked in elsewhere', true), 'checkin-old', array('old'=>$old), 'checkin-old');
+        if (!empty($valid))
+            $this->Session->setFlash(__('Successfully checked in.', true), 'checkin-valid', array('valid'=>$valid), 'checkin-valid');
+        if (!empty($invalid))
+            $this->Session->setFlash(__('Invalid card(s).', true), 'checkin-invalid', array('invalid'=>$invalid), 'checkin-invalid');
+    } else {
+        if (empty($resident['Person']))
+            $this->Session->setFlash(__("Resident ID not found", true));
+        else
+            ///FIXME: Translatable
+            $this->Session->setFlash($resident['Person']['firstName'].' '.$resident['Person']['lastName'].' is not a resident.');
+    }
+    //debug($this->data['Guest']);
+    $this->redirect("/");
+    /*$resident = $this->Identification->findByCardNum($this->data['Resident']['card_num']);
     $guest = $this->Identification->findByCardNum($this->data['Guest']['card_num']);
     $this->data['Entry']['person_id'] = $guest['Person']['id'];
     $this->data['Entry']['resident_id'] = $resident['Resident']['id'];
@@ -14,22 +51,33 @@ class EntriesController extends AppController {
       $this->Session->setFlash('Checked in!');
     else
       $this->Session->setFlash('Failed to check in.');
-    $this->redirect("/entries");
+    $this->redirect("/");*/
   }
   
   public function search() {
-    $this->set('ResidentById', $this->Resident->findAll(array('Identification.card_num'=>$this->data['Resident']['card_num'])));
+    $name = explode(' ', $this->data['Resident']['name'],2);
+    $firstName = '%'.$name[0].'%';
+    if (count($name) == 1)
+        $name[1] = $name[0];
+    $lastName = '%'.$name[1].'%';
+    $name = explode(' ', $this->data['Guest']['name'], 2);
+    $guestFirstName = '%'.$name[0].'%';
+    if (count($name) == 1)
+        $name[1] = $name[0];
+    $guestLastName = '%'.$name[1].'%';
+    $this->set('results', $this->Entry->findAll(array('or'=>array('Identification.card_num'=>$this->data['Resident']['card_num'], 'Identification.card_num' => $this->data['Guest']['card_num']), 'or'=>array('or'=>array('Person.firstName LIKE'=>$firstName, 'Person.lastName LIKE' => $lastName), 'or'=>array('Person.firstName LIKE'=>$guestFirstName, 'Person.lastName LIKE'=>$guestLastName)), 'Resident.room' => $this->data['Resident']['room'])));
     
-    $residents = $this->Person->findAll(array('Person.firstName'=>$this->data['Resident']['name'],'Person.lastName'=>$this->data['Resident']['name']));
-    
-    $residentMatches = array();
+    /*$residentMatches = array();
     foreach($residents as $resident) {
-      $residentMatches = array_merge($residentMatches, $this->Resident->findAll(array('Identification.firstName' => $this->data['Resident']['name'], 'Identification.lastName' => $this->data['Resident']['name'], 'Identification.person_id' => $resident['Person']['id'])));
+      $residentMatches = array_merge($residentMatches, $this->Resident->findAll(array('Identification.person_id' => $resident['Person']['id'])));
     }
     $this->set('Residents', $residentMatches);
     $this->set('RoomResidents', $this->Resident->findAllByRoom($this->data['Resident']['room']));
     $this->set('GuestById', $this->Identification->findAll(array('card_num'=>$this->data['Guest']['card_num'])));
-    $this->set('Guests', $this->Person->findAll(array('firstName'=>$this->data['Guest']['name'], 'lastName'=>$this->data['Guest']['name'])));
+    list($firstName, $lastName) = explode(' ', $this->data['Guest']['name']);
+    $firstName = '%'.$firstName.'%';
+    $lastname = '%'.$lastName.'%';
+    $this->set('Guests', $this->Person->findAll(array('firstName LIKE'=>$firstName, 'lastName LIKE'=>$lastName)));*/
   }
   
   public function index() {
--- views/entries/search.ctp
+++ views/entries/search.ctp
@@ -1,10 +1,14 @@
-<h2>Resident ID</h2>
+<table>
 <?php
-debug($ResidentById);
+echo $html->tableHeaders(array('Guest', 'Resident', 'Building', 'Room', ''));
+foreach($results as $entry) {
+    echo '<tr>';
+    echo '<td><a href="/people/view/'.$entry['Person']['id'].'">'.$entry['Person']['lastName'].', '.$entry['Person']['firstName'].'</a></td>';
+    echo '<td><a href="/residents/view/'.$entry['Resident']['id'].'">'.$entry['Resident']['Identification']['Person']['lastName'].', '.$entry['Resident']['Identification']['Person']['firstName'].'</a></td>';
+    echo '<td><a href="/buildings/view/'.$entry['Resident']['Building']['id'].'">'.$entry['Resident']['Building']['name'].'</a></td>';
+    echo '<td><a href="/buildings/room/'.$entry['Resident']['room'].'">'.$entry['Resident']['room'].'</a></td>';
+    echo '<td><a href="/entries/view/'.$entry['Entry']['id'].'">view</a></td>';
+    echo '</tr>';
+}
 ?>
-<h2>Resident Name</h2>
-debug($Residents);
-debug($RoomResidents);
-debug($GuestById);
-debug($Guests);
-?>
+</table>
