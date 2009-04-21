9fa623f8e224bb6d97d1edc6e0a7ffa321880f97
Trever Fischer <wm161@wm161.net>
2009-04-21 11:52:18 -0700

    app_controller.php: Forgot to commit this. Now the epicenter element isn't broken.

--- app_controller.php
+++ app_controller.php
@@ -1,10 +1,16 @@
 <?php
 class AppController extends Controller {
   var $helpers = array('Javascript', 'Ajax', 'Html', 'Widgets');
+  var $components = array('Auth');
   function constructClasses() {
     $userClass=Configure::read('userBackendModel');
     ClassRegistry::map('User', $userClass);
     Controller::constructClasses();
+  }
+
+  function beforeRender() {
+    $this->set('user', $this->Auth->user());
+    Controller::beforeRender();
   }
 }
 ?>
